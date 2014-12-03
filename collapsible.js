251a41244d7d09d857a3c29e60f416e0e7ad0bf4
Scott Jehl <scottjehl@gmail.com>
2014-12-02 18:29:14 -0600

    use updated enhance binding and trigger it

--- collapsible.js
+++ collapsible.js
@@ -132,9 +132,10 @@
         });
     };
 
-    // Simple auto-init by selector that runs when the dom is ready. Use if desirable.
-    $(function(){
-        $( "." + pluginName )[ pluginName ]();
+    // Simple auto-init by selector that runs when the dom is ready. Trigger "enhance" if desirable.
+    $( document ).bind( "enhance", function( e ){
+        var selector = "." + pluginName;
+        $( $( e.target ).is( selector ) && e.target ).add( selector, e.target ).filter( selector )[ pluginName ]();
     });
 
 })(jQuery, window, document);
