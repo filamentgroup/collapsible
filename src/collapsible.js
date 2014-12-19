a50065c86696b7c889829f12824076db0e4b3e55
Zach Leatherman <zachleatherman@gmail.com>
2014-12-19 14:33:54 -0600

    Tabs police.

--- src/collapsible.js
+++ src/collapsible.js
@@ -7,150 +7,150 @@
 
 ;(function ($, window, document, undefined) {
 
-    // Defaults
-    var pluginName = "collapsible";
-    // overrideable defaults
-    var defaults = {
-        pluginClass: pluginName,
-        collapsedClass: pluginName + "-collapsed",
-        headerClass: pluginName + "-header",
-        contentClass: pluginName + "-content",
-        enhancedClass: pluginName + "-enhanced",
-        instructions: false,
-        collapsed: false
-    };
+	// Defaults
+	var pluginName = "collapsible";
+	// overrideable defaults
+	var defaults = {
+		pluginClass: pluginName,
+		collapsedClass: pluginName + "-collapsed",
+		headerClass: pluginName + "-header",
+		contentClass: pluginName + "-content",
+		enhancedClass: pluginName + "-enhanced",
+		instructions: false,
+		collapsed: false
+	};
 
-    // plugin constructor
-    function Plugin(element, options) {
-        this.element = $( element );
-        var self = this,
-            dataOptions = {};
+	// plugin constructor
+	function Plugin(element, options) {
+		this.element = $( element );
+		var self = this,
+			dataOptions = {};
 
-        // Allow data-attr option setting
-        if( this.element.is( "[data-config]" ) ){
-            for( var option in defaults ){
-                    if( defaults.hasOwnProperty( option) ){
-                    var value = self.element.attr( "data-" + option.replace( /[A-Z]/g, function( c ) {
-                                    return "-" + c.toLowerCase();
-                                }));
+		// Allow data-attr option setting
+		if( this.element.is( "[data-config]" ) ){
+			for( var option in defaults ){
+					if( defaults.hasOwnProperty( option) ){
+					var value = self.element.attr( "data-" + option.replace( /[A-Z]/g, function( c ) {
+									return "-" + c.toLowerCase();
+								}));
 
-                    if ( value !== undefined ) {
-                        if( value === "true" || value === "false" ){
-                            dataOptions[ option ] = value === "true";
-                        }
-                        else {
-                            dataOptions[ option ] = value;
-                        }
-                    }
-                }
-            }
-        }
+					if ( value !== undefined ) {
+						if( value === "true" || value === "false" ){
+							dataOptions[ option ] = value === "true";
+						}
+						else {
+							dataOptions[ option ] = value;
+						}
+					}
+				}
+			}
+		}
 
 
 
-        this.options = $.extend( {}, defaults, dataOptions, options );
+		this.options = $.extend( {}, defaults, dataOptions, options );
 
-        // allow the collapsedClass to set the option if specified
-        if( this.element.is( "." + this.options.collapsedClass ) ){
-            this.options.collapsed= true;
-        }
+		// allow the collapsedClass to set the option if specified
+		if( this.element.is( "." + this.options.collapsedClass ) ){
+			this.options.collapsed= true;
+		}
 
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
 
-    Plugin.prototype = {
-        init: function () {
-            this.header = this.element.children().eq( 0 );
-            this.content = this.header.next();
-            this._addAttributes();
-            this._bindEvents();
-            this.element.trigger( "init" );
-        },
+	Plugin.prototype = {
+		init: function () {
+			this.header = this.element.children().eq( 0 );
+			this.content = this.header.next();
+			this._addAttributes();
+			this._bindEvents();
+			this.element.trigger( "init" );
+		},
 
-        _addAttributes: function(){
-            this.element
-                .addClass( this.options.pluginClass )
-                .addClass( this.options.enhancedClass );
+		_addAttributes: function(){
+			this.element
+				.addClass( this.options.pluginClass )
+				.addClass( this.options.enhancedClass );
 
-            this.header.addClass( this.options.headerClass );
+			this.header.addClass( this.options.headerClass );
 
-            if( this.options.instructions ){
-                this.header.attr( "title", this.options.instructions );
-            }
+			if( this.options.instructions ){
+				this.header.attr( "title", this.options.instructions );
+			}
 
-            this.header.attr( "role", "button" );
+			this.header.attr( "role", "button" );
 
-            this.header.attr( "aria-expanded", "true" );
+			this.header.attr( "aria-expanded", "true" );
 
-            this.header.attr( "tabindex", "0" );
+			this.header.attr( "tabindex", "0" );
 
-            this.content.addClass( this.options.contentClass );
-        },
+			this.content.addClass( this.options.contentClass );
+		},
 
-        _bindEvents: function(){
-            var self = this;
+		_bindEvents: function(){
+			var self = this;
 
-            this.header
-                // use the tappy plugin if it's available
+			this.header
+				// use the tappy plugin if it's available
 		.on( window.tappy ? "tap" : "click", function( e ){
-                    self.toggle();
-		    e.preventDefault();
-                })
-                .on( "keyup", function( e ){
-                    if( e.which === 13 || e.which === 32 ){
-                        self.toggle();
+					self.toggle();
 			e.preventDefault();
-                    }
-                });
+				})
+				.on( "keyup", function( e ){
+					if( e.which === 13 || e.which === 32 ){
+						self.toggle();
+			e.preventDefault();
+					}
+				});
 
-            if( this.options.collapsed ){
-                this.collapse();
-            }
-        },
+			if( this.options.collapsed ){
+				this.collapse();
+			}
+		},
 
-        collapsed: false,
+		collapsed: false,
 
-        expand: function () {
-            var self = $( this ).data( "plugin_" + pluginName ) || this;
-            self.element.removeClass( self.options.collapsedClass );
-            self.collapsed = false;
-            self.header.attr( "aria-expanded", "true" );
-            self.element.trigger( "expand" );
-        },
+		expand: function () {
+			var self = $( this ).data( "plugin_" + pluginName ) || this;
+			self.element.removeClass( self.options.collapsedClass );
+			self.collapsed = false;
+			self.header.attr( "aria-expanded", "true" );
+			self.element.trigger( "expand" );
+		},
 
-        collapse: function() {
-            var self = $( this ).data( "plugin_" + pluginName ) || this;
-            self.element.addClass( self.options.collapsedClass );
-            self.collapsed = true;
-            self.header.attr( "aria-expanded", "false" );
-            self.element.trigger( "collapse" );
-        },
+		collapse: function() {
+			var self = $( this ).data( "plugin_" + pluginName ) || this;
+			self.element.addClass( self.options.collapsedClass );
+			self.collapsed = true;
+			self.header.attr( "aria-expanded", "false" );
+			self.element.trigger( "collapse" );
+		},
 
-        toggle: function(){
-            if(  this.collapsed ){
-                this.expand();
-            } else {
-                this.collapse();
-            }
-        }
-    };
+		toggle: function(){
+			if(  this.collapsed ){
+				this.expand();
+			} else {
+				this.collapse();
+			}
+		}
+	};
 
-    // lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[ pluginName ] = function (options) {
-        return this.each(function () {
-            if ( !$( this ).data( "plugin_" + pluginName ) ) {
-                $( this ).data( "plugin_" + pluginName, new Plugin( this, options ));
-            }
-        });
-    };
+	// lightweight plugin wrapper around the constructor,
+	// preventing against multiple instantiations
+	$.fn[ pluginName ] = function (options) {
+		return this.each(function () {
+			if ( !$( this ).data( "plugin_" + pluginName ) ) {
+				$( this ).data( "plugin_" + pluginName, new Plugin( this, options ));
+			}
+		});
+	};
 
-    // Simple auto-init by selector that runs when the dom is ready. Trigger "enhance" if desirable.
-    $( document ).bind( "enhance", function( e ){
-        var selector = "." + pluginName;
-        $( $( e.target ).is( selector ) && e.target ).add( selector, e.target ).filter( selector )[ pluginName ]();
-    });
+	// Simple auto-init by selector that runs when the dom is ready. Trigger "enhance" if desirable.
+	$( document ).bind( "enhance", function( e ){
+		var selector = "." + pluginName;
+		$( $( e.target ).is( selector ) && e.target ).add( selector, e.target ).filter( selector )[ pluginName ]();
+	});
 
 })(jQuery, window, document);
