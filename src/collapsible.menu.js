a51f504ee26b772445cc4f5ea18d0e5ef3b303bc
Scott Jehl <scottjehl@gmail.com>
2014-12-23 10:50:57 -0600

    bring back collapsible trigger handling. this allows a sibling element to act as a trigger on hover for expanding the collapsible, yet still allows a default tap/click on it

--- src/collapsible.menu.js
+++ src/collapsible.menu.js
@@ -11,6 +11,8 @@
 		var pluginName = "collapsible";
 		if( $( e.target ).is( "." + pluginName ) ){
 			var $collapsible = $( e.target );
+			var $trigger = $collapsible.prev().filter( "." + pluginName + "-trigger" );
+			var triggerExpand = pluginName + "-trigger-expand";
 
 			function isMenu(){
 				return $collapsible.find( "." + pluginName + "-content" ).css( "position" ) === "absolute";
@@ -23,7 +25,7 @@
 			// tapout/clickout behavior
 			$( document ).bind( "touchstart." + pluginName + " click." + pluginName, function( a ){
 				// if the event target is not in the collapsible, and the collapsible is expanded, and it's a menu presentation... collapse it!
-				if( !$( a.target ).closest( e.target ).length && !$collapsible.data( pluginName ).collapsed && isMenu ){
+				if( !$( a.target ).closest( e.target ).length && !$( a.target ).closest( $trigger ).length && !$collapsible.data( pluginName ).collapsed && isMenu ){
 					setTimeout(function(){
 						$collapsible.data( pluginName ).collapse();
 					});
@@ -31,17 +33,32 @@
 				}
 			} );
 
-			$collapsible
-				.bind( "mouseenter." + pluginName, function(){
-					if( isMenu() ){
-						$collapsible.data( pluginName ).expand();
-					}
-				} )
-				.bind( "mouseleave." + pluginName, function(){
-					if( isMenu() ){
-						$collapsible.data( pluginName ).collapse();
-					}
-				} );
+			// hover behavior for collapsibles and triggers relies on the presence of data-collapsible-hover attr
+			if( $collapsible.is( "[data-collapsible-hover]" ) ){
+
+				$collapsible
+					.add( $trigger )
+					.bind( "mouseenter." + pluginName, function(){
+						if( isMenu() ){
+							$collapsible.data( pluginName ).expand();
+						}
+					} )
+					.bind( "mouseleave." + pluginName, function(){
+						if( isMenu() ){
+							$collapsible.data( pluginName ).collapse();
+						}
+					} );
+
+				$collapsible
+					.bind( "expand", function(){
+						$trigger.addClass( triggerExpand );
+					} )
+					.bind( "collapse", function(){
+						$trigger.removeClass( triggerExpand );
+					} );
+			}
+
+
 		}
 	} );
 
