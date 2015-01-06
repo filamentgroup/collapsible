521f3ec484a4cdfa0a5ce34dc37b683838fafefd
Scott Jehl <scottjehl@gmail.com>
2015-01-06 17:30:50 -0600

    move all arrow key handling to the menu plugin. fixes #15

--- src/collapsible.js
+++ src/collapsible.js
@@ -117,23 +117,6 @@
 					}
 				});
 
-			// arrow key handling applies to the entire collapsible
-			this.element
-				.bind( "keydown." + pluginName, function( e ){
-					// arrow key behavior: collapsible must be expanded to accept arrow navigation
-					if( !self.collapsed ){
-						if( e.which === 39 || e.which === 40 ){
-							self.arrow( e.target );
-							e.preventDefault();
-						}
-						else if( e.which === 37 || e.which === 38 ){
-							self.arrow( e.target, true );
-							e.preventDefault();
-						}
-					}
-
-				});
-
 			if( this.options.collapsed ){
 				this.collapse( false );
 			}
@@ -170,61 +153,9 @@
 			}
 		},
 
-		focusable: "a, input, textarea, select, button, [tabindex='0']",
+		focusable: "a, input, textarea, select, button, [tabindex='0']"
 
-		// arrow method handles the arrow key navigation, which largely maps to the tab key within the component
-		arrow: function( target, back ){
-			var self = this;
-			// find all focusables in this collapsible content area
-			var $focusables = this.content.find( this.focusable );
-			// nextTab will be the next element receiving focus from this arrow keydown
-			var nextTab;
 
-			// if the keydown target is the header and it's down or right, focus on the first focusable
-			if( $( target ).is( this.header ) && !back ){
-				nextTab = $focusables[ 0 ];
-			}
-			else {
-				// if it's a backward arrow, reverse the array
-				if( back ){
-					// this if can go away once https://github.com/filamentgroup/shoestring/issues/80 is fixed
-					// check if already an array (shoestring with bug above)
-					if( $focusables.reverse ){
-						$focusables = $focusables.reverse();
-					}
-					// otherwisejquery will need a get()
-					else {
-						$focusables = $( $focusables.get().reverse() );
-					}
-				}
-				// afterTarget becomes true once the target has been passed in the each loop
-				var afterTarget = false;
-				// loop focusables
-				$focusables.each(function( i ){
-					// if nextTab isn't defined yet, we're after the target in the loop, and the target appears to be displayed
-					// NOTE: the offset checks replaced the following, which tied keyboard behavior to aria state:
-						// !$( this ).closest( ".collapsible-collapsed .collapsible-content" ).length
-						// unfortunately, we sometimes display visually elements that are still aria-hidden.
-						// The current check caters to sighted keyboard users over non-sighted keyboard users. TODO: figure this out.
-					if( !nextTab && afterTarget && this.offsetHeight > 0 && this.offsetLeft > -1 ){
-						nextTab = this;
-					}
-					// try to set afterTarget if not already set
-					if( !afterTarget ) {
-						afterTarget = $( this ).is( target );
-					}
-				});
-			}
-
-			// if we have a next element to send focus to at this point, do that. Otherwise, focus back on header
-			if( nextTab ){
-				nextTab.focus();
-			}
-			else {
-				// no nextTab? focus back to header
-				this.header[ 0 ].focus();
-			}
-		}
 
 	};
 
--- src/collapsible.menu.js
+++ src/collapsible.menu.js
@@ -11,6 +11,7 @@
 		var pluginName = "collapsible";
 		if( $( e.target ).is( "." + pluginName ) ){
 			var $collapsible = $( e.target );
+			var self = $collapsible.data( pluginName );
 			var $trigger = $collapsible.prev().filter( "." + pluginName + "-trigger" );
 			var triggerExpand = pluginName + "-trigger-expand";
 
@@ -57,6 +58,80 @@
 						$trigger.removeClass( triggerExpand );
 					} );
 			}
+
+
+			// add keyboard/arrow handling
+			// arrow method handles the arrow key navigation, which largely maps to the tab key within the component
+			self.arrow = function( target, back ){
+
+				// find all focusables in this collapsible content area
+				var $focusables = this.content.find( this.focusable );
+				// nextTab will be the next element receiving focus from this arrow keydown
+				var nextTab;
+
+				// if the keydown target is the header and it's down or right, focus on the first focusable
+				if( $( target ).is( this.header ) && !back ){
+					nextTab = $focusables[ 0 ];
+				}
+				else {
+					// if it's a backward arrow, reverse the array
+					if( back ){
+						// this if can go away once https://github.com/filamentgroup/shoestring/issues/80 is fixed
+						// check if already an array (shoestring with bug above)
+						if( $focusables.reverse ){
+							$focusables = $focusables.reverse();
+						}
+						// otherwisejquery will need a get()
+						else {
+							$focusables = $( $focusables.get().reverse() );
+						}
+					}
+					// afterTarget becomes true once the target has been passed in the each loop
+					var afterTarget = false;
+					// loop focusables
+					$focusables.each(function( i ){
+						// if nextTab isn't defined yet, we're after the target in the loop, and the target appears to be displayed
+						// NOTE: the offset checks replaced the following, which tied keyboard behavior to aria state:
+							// !$( this ).closest( ".collapsible-collapsed .collapsible-content" ).length
+							// unfortunately, we sometimes display visually elements that are still aria-hidden.
+							// The current check caters to sighted keyboard users over non-sighted keyboard users. TODO: figure this out.
+						if( !nextTab && afterTarget && this.offsetHeight > 0 && this.offsetLeft > -1 ){
+							nextTab = this;
+						}
+						// try to set afterTarget if not already set
+						if( !afterTarget ) {
+							afterTarget = $( this ).is( target );
+						}
+					});
+				}
+
+				// if we have a next element to send focus to at this point, do that. Otherwise, focus back on header
+				if( nextTab ){
+					nextTab.focus();
+				}
+				else {
+					// no nextTab? focus back to header
+					this.header[ 0 ].focus();
+				}
+			};
+
+			// bind keydown handlers.
+			// arrow key handling applies to the entire collapsible
+			self.element
+				.bind( "keydown." + pluginName, function( e ){
+					// arrow key behavior: collapsible must be expanded to accept arrow navigation
+					if( !self.collapsed ){
+						if( e.which === 39 || e.which === 40 ){
+							self.arrow( e.target );
+							e.preventDefault();
+						}
+						else if( e.which === 37 || e.which === 38 ){
+							self.arrow( e.target, true );
+							e.preventDefault();
+						}
+					}
+
+				});
 
 
 		}
