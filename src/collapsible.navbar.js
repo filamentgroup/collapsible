ae73e593fda06ebdfe34e37bb3ad32b08348325c
Scott Jehl <scottjehl@gmail.com>
2016-09-27 13:04:12 -0500

    rove focus to first 'menu' item in a navbar on expand, for keyboard support

--- src/collapsible.navbar.js
+++ src/collapsible.navbar.js
@@ -16,7 +16,7 @@
   			var $navItems = $collapsible.find( "." + itemClass );
         var $moreBtn = $collapsible.find( "." + itemMoreClass );
 
-        $moreBtn.attr( "aria-label", "Navigation" );
+        $moreBtn.attr( "aria-label", "More items" );
 
         var resetItems = function(){
           $moreBtn.removeClass( itemMoreHiddenClass );
@@ -66,6 +66,10 @@
         $( w ).bind( "resize", initItems );
 
         $collapsible
+        .bind( "expand", function( e ){
+          if( $( e.target ).is( $collapsible )  )
+            $collapsible.find( "." + itemMenuClass + " a" ).eq( 0 ).focus();
+        })
         .bind( "mouseover." + pluginName, function( e ){
 						if( $collapsible.is( "[data-collapsible-hover]" ) && !$( e.target ).closest( "." + itemMenuClass + ", ." + itemMoreClass ).length ){
 							$collapsible.data( pluginName ).collapse();
